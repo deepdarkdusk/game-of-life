15c4b86ffb986984a240a0c7ccf8db335b9f715b
Lorenz Werner <lwerner@bunnybase.de>
2016-01-07 15:03:16 +0100

    import legacy code :)

---
+++ game_of_life.js
@@ -0,0 +1,423 @@
+var game_fields       = 0;
+var game_field_width  = 0;
+var game_field_height = 0;
+
+var game_timeout      = false;
+var game_active_level = false;
+
+// this function draws the control pannel
+function draw_panel( id ) {
+	var outer_div = document.createElement("div");
+	outer_div.id = id + "_panel";
+
+	var step_button = document.createElement("input");
+	step_button.type  = "submit";
+	step_button.value = "step";
+	step_button.setAttribute ( "onclick", "javascript: execute_step('"+ id +"');" );
+
+	outer_div.appendChild( step_button );
+
+	return outer_div;
+
+}
+// this function draws the playing field
+function draw_field( id, width, height ) {
+	if( width * height > 2500 ) {
+		if( ! confirm( "Resizing to fields larger than 2500 cells can slow down your PC or will freeze your browser. Do you really wan't to proceed?" ) ) return;
+	}
+	
+	// unset current content
+	while( document.getElementById(id).firstChild ) {
+		document.getElementById(id).removeChild( document.getElementById(id).firstChild );
+	}
+
+	// store width and height in global vars
+	game_field_width  = width;
+	game_field_height = height;
+
+	// start creating the playing field table
+	var table = document.createElement("table");
+	table.id  = id + "_panel";
+	table.setAttribute("cellpadding", 0);
+	table.setAttribute("cellspacing", 0);
+
+	var i = 0;
+	for( var y = 0; y < height; y++  ) {
+		// start new table row
+		var tr = document.createElement("tr");
+		
+		for( var x = 0; x < width; x++ ) {
+			i++;
+			var td = document.createElement("td");
+			td.className    = "fields_game_unchecked";
+			td.id           = id + "_td_" + i;
+			// td.title        = i;
+
+			var img = document.createElement("img");
+			img.setAttribute ( "onclick", "javascript: switch_td_state( document.getElementById('"+ id +"_td_"+ i +"'), true );" );
+			img.src       = "game_of_life.png";
+			img.className = "fields_game_img"
+
+			td.appendChild( img );
+			tr.appendChild( td );
+		
+		}
+
+		table.appendChild( tr );
+
+	}
+
+	game_fields = i;
+
+	return table;
+}
+function switch_td_state( element, click ) {
+	// if riddle mode is active, we have to check if the player still has an placeable cell left
+	if( game_active_level !== false && click == true && element.className == "fields_game_unchecked" ) {
+		var clicks = document.getElementById('game_panel_clicks');
+		if( clicks.value > 0 ) {
+			clicks.value--; 
+
+		// if the player has none cell left to activate
+		// leave this function
+		} else {
+			// alert("You have no cells left!");
+			return false;
+
+		}
+				
+	// if riddle mode is active, the player can only activate cells
+	// he cannot deactivate any cell
+	} else if( game_active_level !== false && click == true ) {
+		return false;
+
+	}
+
+	// change cell status
+	if( element.className == "fields_game_checked" ) {
+		var _class = "fields_game_unchecked";
+	} else {
+		var _class = "fields_game_checked";
+	}
+	element.className = _class;
+
+	// in riddle mode, we have "push" the start button because the player will not see it
+	if( game_active_level !== false && click == true ) {
+		if( clicks.value == 0 ) {
+			// alert("You have no cells left!");
+			var x = function() {
+				eval( document.getElementById('game_panel_start').getAttribute("onclick") );
+			}
+			window.setTimeout( x, 500 );
+		}
+	}
+
+}
+function execute_step( id ) {
+	var cells  = new Array;
+	for( var i = 1; i <= game_fields; i++ ) {
+		// first we have to determine which cells are connected to the active cell
+		var neighbors = new Array;
+		neighbors[0] = ( parseInt(i) - parseInt(game_field_width) );      // upper center
+		neighbors[1] = ( parseInt(i) - parseInt(game_field_width) - 1 );  // upper left
+		neighbors[2] = ( parseInt(i) - parseInt(game_field_width) + 1 );  // upper right
+		neighbors[3] = ( parseInt(i) + 1 );                               // right
+		neighbors[4] = ( parseInt(i) - 1 );                               // left
+		neighbors[5] = ( parseInt(i) + parseInt(game_field_width) - 1 );  // lower left
+		neighbors[6] = ( parseInt(i) + parseInt(game_field_width) + 1 );  // lower right
+		neighbors[7] = ( parseInt(i) + parseInt(game_field_width) );      // lower center
+		neighbors[8] = parseInt(i);                                       // active cell
+
+		// we need to sort these cells out that have to be in the same row to be connected
+		// if the outer cell is in an other row than the inner cell, the outer cell will be
+		// set to -1 and so it remains unchecked
+		var overflow = new Array(
+			new Array( 8, 3 ), 
+			new Array( 8, 4 ), 
+			new Array( 0, 1 ), 
+			new Array( 0, 2 ), 
+			new Array( 7, 5 ),
+			new Array( 7, 6 )
+		);
+		for( var j in overflow ) {
+			var row1 = Math.floor( neighbors[overflow[j][0]] / parseInt(game_field_width) - 0.00000000001 );
+			var row2 = Math.floor( neighbors[overflow[j][1]] / parseInt(game_field_width) - 0.00000000001 );
+
+			if( row1 != row2 ) {
+				neighbors[overflow[j][1]] = -1;
+
+			}
+
+		}
+
+		var connected = 0;
+
+		// checking an living cell
+		if( document.getElementById( id + "_td_" + i ).className == "fields_game_checked" ) {
+			// check all 8 neighbors for active cells
+			for( var j = 0; j < 8; j++ ) {
+				if( 
+					neighbors[j] > 0 &&
+					neighbors[j] <= game_fields &&
+					document.getElementById( id + "_td_" + neighbors[j] ).className == "fields_game_checked"
+				) {
+					connected++;
+					
+				}
+				
+			}
+			// cell dies if it has less than 2 or more than 3 active neighbors
+			if( connected < 2 || connected > 3 ) {
+				cells[i] = i;
+
+			} 
+
+
+		// checking an dead cell
+		} else {
+			for( var j = 0; j < 8; j++ ) {
+				if( 
+					neighbors[j] > 0 &&
+					neighbors[j] <= game_fields &&
+					document.getElementById( id + "_td_" + neighbors[j] ).className == "fields_game_checked"
+				) {
+					connected++;
+					
+				}
+				
+			}
+			// cell lives if it has exactly 3 living neighbors
+			if( connected == 3 ) {
+				cells[i] = i;
+
+			} 
+			
+
+		}
+
+	}
+
+	// count changed fields
+	var changes = 0;
+	for( var i = 1; i <= game_fields; i++ ) {	
+		if( cells[i] != undefined ) {
+			changes++;
+		}
+
+	}
+
+	// switch cell state of changed
+	for( var i in cells ) {	
+		var cell = document.getElementById( id +"_td_"+ cells[i] );
+		if( cells[i] != undefined ) {
+			switch_td_state( cell );
+		}
+
+	}
+
+	return changes;
+}
+// die schleife
+function step_loop( id, timeout ) {
+	if( timeout < 100 ) {
+		alert( 'The Timout has to be at least 100 milliseconds.' );
+		return;
+	}
+
+	/* 
+	 * this shouldn't be needed anymore, since the player can't see the start button
+	 *
+	if( game_active_level !== false ) {
+		if( document.getElementById('game_panel_clicks').value > 0 ) {
+			alert( 'You have to use all clicks.' );
+			return;
+		}
+	}
+	*/
+
+	// switch start button to stop button
+	document.getElementById(id+'_panel_start').value = 'stop';
+	document.getElementById(id+'_panel_start').setAttribute( "onclick", "window.clearTimeout(game_timeout); game_timeout = false; document.getElementById('game_panel_rounds').value = 0; step_loop('"+ id +"', 100, 0);" );
+
+	var steps = document.getElementById(id+'_panel_rounds');
+	if( steps.value > 0 ) {
+		// if no changes where made in the round we set
+		// the remaining rounds (steps) to zero
+		if( ! execute_step(id) ) {
+			steps.value = 0;
+
+		// if changes where made in this round we decrement
+		// the steps value
+		} else {
+			steps.value--;
+
+		}
+
+		// start next round after "timeout" milliseconds
+		game_timeout = window.setTimeout('step_loop("'+ id +'", '+ timeout +');', timeout);
+		document.getElementById('game_panel_living_cells').value = get_living_cells( "game" );
+
+	// if there are no more rounds left
+	} else {
+		// set game_timeout to false
+		game_timeout = false;
+
+		// switch stop button to start button
+		document.getElementById(id+'_panel_start').value  = 'start';
+		document.getElementById(id+'_panel_start').setAttribute( "onclick", "javascript: step_loop( 'game', document.getElementById('game_panel_timeout').value, document.getElementById('game_panel_rounds').value );" );
+
+		// if player is in riddle mode we have to check
+		// wheter the level was passed or failed
+		if( game_active_level !== false ) {
+			check_level();
+		
+		}
+	}
+
+}
+// this function randomizes the cells on the playing field
+function randomize_cells(id) {
+	reset_cells(id);
+	for( var i = 1; i <= game_fields; i++ ) {	
+		if( ( Math.random() * 100 ) > ( Math.random() * 20 + 70 ) ) {
+			var cell = document.getElementById( id +"_td_"+ i );
+			switch_td_state( cell );
+		}
+
+	}
+
+	alert('Cells have been randomized.');
+	
+}
+// this function resets all cells to be inactive
+function reset_cells(id) {
+	for( var i = 1; i <= game_fields; i++ ) {	
+		document.getElementById( id +"_td_"+ i ).className = "fields_game_unchecked";
+
+	}
+	
+	alert('All cells have been resetted.');
+
+}
+// this function returns the count of living cells
+function get_living_cells(id) {
+	var count = 0;
+	for( var i = 1; i <= game_fields; i++ ) {	
+		if( document.getElementById( id +"_td_"+ i ).className == "fields_game_checked" ) {
+			count++;
+		}
+
+	}
+	return count;
+	
+}
+// this function returns the coordinates of all active cells
+function get_living_cell_coordinates() {
+	var count = 0;
+	var cells = new Array();
+	for( var i = 1; i <= game_fields; i++ ) {	
+		if( document.getElementById( "game_td_"+ i ).className == "fields_game_checked" ) {
+			cells[count++] = i;
+		}
+
+	}
+	return cells;
+	
+}
+
+// this function loads an level
+// to start the game simply run this function with level 1
+// eg: load_level(1);
+function load_level(level) {
+	// if the level is found, load it and apply it
+	if( game_levels[(level-1)] != undefined ) {
+		game_active_level = (level-1);
+
+		// resize playing field
+		var playing_field = draw_field("game", game_levels[game_active_level][0][0], game_levels[game_active_level][0][1]);
+		document.getElementById('game').appendChild( playing_field );
+
+		// activate cells
+		for( var i in game_levels[game_active_level][1] ) {
+			document.getElementById('game_td_' + game_levels[game_active_level][1][i] ).className = "fields_game_checked";
+
+		}
+
+		document.getElementById('game_panel_rounds').value         = game_levels[game_active_level][2];
+		document.getElementById('game_panel_rounds').readOnly      = "readonly";
+		document.getElementById('game_panel_clicks').value         = game_levels[game_active_level][3];
+		document.getElementById('game_panel_level_goal').innerHTML = game_levels[game_active_level][5];
+		document.getElementById('game_panel_active_level').value   = ( game_active_level + 1 );
+		document.getElementById('game_panel_living_cells').value   = get_living_cells( "game" );
+
+		alert('Level '+level+' loaded. Have Fun!');
+
+		return true;
+
+	// if the level is not found we display the game ends message
+	} else {
+		alert('Congratulation! You have mastered all challenges!<br />GAME OVER! <a href="javascript: void(0);" onclick="javascript: load_level(1);">Click here to restart this game!</a>');
+
+		return false;
+
+	}
+}
+function check_level() {
+	if( game_levels[game_active_level] != undefined ) {
+		if( game_levels[game_active_level][4]() ) {
+			alert('Level '+( game_active_level +1 )+' has been solved! Congratulations!');
+			window.setTimeout( "load_level( "+( game_active_level +2 )+" )", 2500 );
+
+		} else {
+			alert('You failed solving level '+( game_active_level +1 )+'! Try Again!');
+			window.setTimeout( "load_level( "+( game_active_level +1 )+" )", 1500 );
+
+		}
+
+	} else {
+		alert('There is an Error.');				
+	}
+
+	return;
+
+}
+
+function switch_mode( element ) {
+	window.clearTimeout( game_timeout );
+	game_active_level = false;
+	game_timeout      = false;
+	
+	document.getElementById('game_panel_rounds').value = 100;
+
+	var start_button = document.getElementById('game_panel_start');
+	start_button.value = "start";
+	start_button.setAttribute( "onclick", "javascript: step_loop( 'game', document.getElementById('game_panel_timeout').value, document.getElementById('game_panel_rounds').value );")
+
+	if( element.value == "sandbox" ) {
+		element.value = "riddle";
+		document.getElementById('game_panel_menu_sandbox').style.display = 'inline';
+		document.getElementById('game_panel_menu_riddle').style.display  = 'none';
+		document.getElementById('game_panel_rounds').readOnly            = false;
+
+		alert( "Switched to Sandbox-Mode!" );
+
+	} else {
+		element.value = "sandbox";
+
+		document.getElementById('game_panel_menu_sandbox').style.display = 'none';
+		document.getElementById('game_panel_menu_riddle').style.display  = 'inline';
+
+		load_level(1);
+
+	}
+
+}
+function set_message( msg ) {
+	document.getElementById('game_message').innerHTML = msg;
+
+}
+function alert( msg ) {
+	set_message( msg );
+
+}
+
---
+++ game_of_life.levels.js
@@ -0,0 +1,212 @@
+var game_levels = new Array(
+	new Array(
+		// width / height
+		new Array( 30, 30 ),
+		// active cells
+		new Array( 404,405,434 ),
+		// level rounds
+		5,
+		// allowed clicks
+		1,
+		// living cells level goal check function 
+		function() { return get_living_cells('game') == 4; },
+		// human readable level description
+		"exactly 4 active cells after 5 rounds"
+	),
+	new Array(
+		// width / height
+		new Array( 30, 30 ),
+		// active cells
+		new Array( 340,341,342,343,344,369,375,398,406 ),
+		// level rounds
+		10,
+		// allowed clicks
+		2,
+		// living cells level goal check function 
+		function() { return get_living_cells('game') == 0; },
+		// human readable level description
+		"no active cells after 10 rounds"
+	),
+	new Array(
+		// width / height
+		new Array( 30, 30 ),
+		// active cells
+		new Array( 340,341,342,343,344,369,375,398,406 ),
+		// level rounds
+		10,
+		// allowed clicks
+		2,
+		// living cells level goal check function 
+		function() { return get_living_cells('game') == 8; },
+		// human readable level description
+		"exactly 8 active cells after 10 rounds"
+	),
+	new Array(
+		// width / height
+		new Array( 30, 30 ),
+		// active cells
+		new Array( 282,286,312,316,342,344,346,372,376 ),
+		// level rounds
+		30,
+		// allowed clicks
+		2,
+		// living cells level goal check function 
+		function() { return get_living_cells('game') == 8; },
+		// human readable level description
+		"exactly 8 active cells after 30 rounds"
+	),
+	new Array(
+		// width / height
+		new Array( 30, 30 ),
+		// active cells
+		new Array( 32,58,59,62,839,842,843,869 ),
+		// level rounds
+		3,
+		// allowed clicks
+		4,
+		// living cells level goal check function 
+		function() { return get_living_cells('game') == 9; },
+		// human readable level description
+		"exact 9 active cells after 3 rounds"
+	),
+	new Array(
+		// width / height
+		new Array( 10, 10 ),
+		// active cells
+		new Array( 28,29,30 ),
+		// level rounds
+		35,
+		// allowed clicks
+		2,
+		// living cells level goal check function 
+		function() { return get_living_cells('game') == 4; },
+		// human readable level description
+		"exactly 4 active cells after 35 rounds"
+	),
+	new Array(
+		// width / height
+		new Array( 30, 30 ),
+		// active cells
+		new Array( 30,59,842,871 ),
+		// level rounds
+		40,
+		// allowed clicks
+		6,
+		// living cells level goal check function 
+		function() { return get_living_cells('game') == 80; },
+		// human readable level description
+		"exactly 80 active cells after 40 rounds"
+	),
+	new Array(
+		// width / height
+		new Array( 40, 40 ),
+		// active cells
+		new Array(
+			496,500,501,502,503,504,536,540,576,580,616,620,656,657,658,659,
+			660,661,662,663,664,700,704,740,744,780,784,816,817,818,819,820,824
+		),
+		// level rounds
+		20,
+		// allowed clicks
+		1,
+		// living cells level goal check function 
+		function() { return get_living_cells('game') == 18; },
+		// human readable level description
+		"exactly 18 active cells after 20 rounds"
+	),
+	new Array(
+		// width / height
+		new Array( 30, 30 ),
+		// active cells
+		new Array( 340,341,342,343,344,369,375,398,401,406,428,436,459,465,490,491,492,493,494 ),
+		// level rounds
+		10,
+		// allowed clicks
+		2,
+		// living cells level goal check function 
+		function() { return get_living_cells('game') == 15; },
+		// human readable level description
+		"exactly 15 active cells after 10 rounds"
+	),
+	new Array(
+		// width / height
+		new Array( 50, 30 ),
+		// active cells
+		new Array( 
+			120,121,122,123,124,168,169,170,174,175,176,217,227,267,277,316,328,366,378,416,428,467,468,469,475,476,477,
+			519,525,567,569,575,577,617,619,620,621,622,623,624,625,627,667,677,717,718,719,720,721,722,723,724,725,726,727
+		),
+		// level rounds
+		10,
+		// allowed clicks
+		10,
+		// living cells level goal check function 
+		function() { return get_living_cells('game') < 80; },
+		// human readable level description
+		"less than 80 active cells after 10 rounds"
+	),
+	new Array(
+		// width / height
+		new Array( 16, 16 ),
+		// active cells
+		new Array(
+			18,19,24,25,30,31,34,47,52,54,59,61,69,76,84,86,91,93,114,120,121,127,130,136,
+			137,143,164,166,171,173,181,188,196,198,203,205,210,223,226,227,232,233,238,239
+		),
+		// level rounds
+		8,
+		// allowed clicks
+		9,
+		// living cells level goal check function 
+		function() { return get_living_cells('game') == 24; },
+		// human readable level description
+		"exactly 24 active cells after 8 rounds"
+	),
+	new Array(
+		// width / height
+		new Array( 16, 16 ),
+		// active cells
+		new Array( 16,31,46,47,48,120,121,136,137,209,210,211,226,241 ),
+		// level rounds
+		20,
+		// allowed clicks
+		2,
+		// living cells level goal check function 
+		function() { return get_living_cells('game') == 4; },
+		// human readable level description
+		"exactly 4 active cells after 20 rounds"
+	),
+	new Array(
+		// width / height
+		new Array( 16, 16 ),
+		// active cells
+		new Array( 16,31,46,47,48,120,121,136,137,209,210,211,226,241 ),
+		// level rounds
+		20,
+		// allowed clicks
+		2,
+		// living cells level goal check function 
+		function() { return get_living_cells('game') == 46; },
+		// human readable level description
+		"exactly 46 active cells after 20 rounds"
+	),
+	// final level
+	new Array(
+		// width / height
+		new Array( 70, 20 ),
+		// active cells
+		new Array(
+			5,6,7,11,12,16,20,23,24,25,74,80,83,86,87,89,90,93,144,146,147,150,151,152,153,156,158,160,163,164,214,217,220,223,226,
+			230,233,285,286,287,290,293,296,300,303,304,305,873,874,878,882,885,886,887,890,891,897,942,945,948,952,955,960,962,967,
+			1012,1015,1019,1021,1025,1026,1030,1031,1037,1082,1085,1089,1091,1095,1100,1102,1153,1154,1160,1165,1166,1167,1170,1172
+		),
+		// level rounds
+		15,
+		// allowed clicks
+		1,
+		// living cells level goal check function 
+		function() { return get_living_cells('game') == 86; },
+		// human readable level description
+		"exact 86 active cells after 15 rounds"
+	)
+);
